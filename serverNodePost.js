924c0507717183601434ca2d12425f9f4fe905d3
LuisMarmol <luis.marmol@tecnologicoliceocristiano.edu.ec>
2024-04-23 19:12:33 -0500

    Se añadieron labels para User y Password e input para la contraseña.

--- serverNodePost.js
+++ serverNodePost.js
@@ -16,17 +16,43 @@ const server = servidor.createServer((req, res) => {
         });
 
         req.on('end', () => {
+            
+            let userData = body.split('&');
+            let userName = decodeURIComponent(userData[0].split('=')[1]); //El método decorURIComponent sirve para decodificar componentes de un Uniform Resource Identifier
+            let password = decodeURIComponent(userData[1].split('=')[1]); //En este caso, el formulario está codificado en formato aplication/x-www-form-urlencode
+            //Este formato es el más común cuando hablamos de enviar datos desde un formalario de tipo HTML hacia un servidor.
+
+            res.setHeader('Content-Type', 'text/html'); //Establece el encabezado de nuestro servidor, y le estamos diciendo que estamos enviando contenido de texto de HTML.
+            res.end(`<p>Username: ${userName}</p><p>Password: ${password}</p>`); //El ${userName} y ${password} son una interpolación de JavaScript.
+            //Donde tanto ${userName} como ${password} serán reemplazados por los valores respectivos de userName y password.
+            
+            /*Código de la clase
+            {Username=David}
+            {Password=Contraseña}
             let userName = body.split
             ('=')[1];
             
             res.end(userName);
+            */
         });
     }
     
     else {
         res.setHeader('Content-Type', 'text/html');
     
-        res.end('<form method="POST"><input type="text" name="username"></input><button type="submit">Create User</button></form>');    
+        res.end(`<form method="POST">
+                    <label for="username">Username: </label>
+                    <input type="text" name="username"></input>
+
+                    <br>
+
+                    <label for="password">Password: </label>
+                    <input type="text" name="password"></input>
+
+                    <br>
+
+                    <button type="submit">Create User</button></form>`
+                );    
     }
 });
 
